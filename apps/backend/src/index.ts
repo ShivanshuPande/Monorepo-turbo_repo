59bf1086387125229bd80892b068157897266468
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-23 00:57:34 +0530

    final changes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import {BACKEND_URL} from "@repo/common/config"
 const app = express();
-
-
-
-app.use(express.json())
+const BACKEND_URLReal = BACKEND_URL
+app.use(express.json());
+const port:number = 3666;
 
 app.get("/" , (req ,res)=>{
     res.json({
-        message : "Hello world"
+        message : "critical issue solved"
     })
-})
+});
+console.log(BACKEND_URLReal)
+console.log(`The web backend is running on ${port}`);
 
-app.listen(3666);
+app.listen(port , ()=>{
+    `app is running on the port ${port}`
+});
